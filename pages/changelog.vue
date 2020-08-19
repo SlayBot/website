2696246828162b1b45e1e479e28db3e964ae8f15
GhostSlayer <aleksi.klen@gmail.com>
2020-08-19 21:55:55 +0300

    Added changelog page

---
+++ pages/changelog.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="container">
+  <h1 class="col s12">Changelog</h1>
+  <div class="col s12 changelog">
+    <div class="card-changelog">
+        <div class="card-content-changelog white-text" style="margin:1,2%">
+            <span class="card-title-changelog" style="margin-bottom: 0;">SlayBot Version 0.1.4</span>
+            <span class="discord-text text-greyple" style="margin-bottom: 8px; display: block;">18th of August 2020</span>
+            <p>Updates</p>
+            <p>
+            • Added Environment field to the <code>stats</code> command
+            </p>
+        </div>
+    </div>
+    <div class="card-changelog">
+        <div class="card-content-changelog white-text" style="margin:1,2%">
+            <span class="card-title-changelog" style="margin-bottom: 0;">SlayBot Version 0.1.3</span>
+            <span class="discord-text text-greyple" style="margin-bottom: 8px; display: block;">16th of August 2020</span>
+            <p>Fixed</p>
+            <p>
+            • Fixed <code>bug</code> and <code>suggestion</code> commands
+            </p>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Changelog',
+  },
+}
+</script>
