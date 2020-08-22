ab2731bda7a2022d2b585237d9d6668b3d2232ee
GhostSlayer <aleksi.klen@gmail.com>
2020-08-22 21:25:32 +0300

    Update changelog

--- pages/changelog.vue
+++ pages/changelog.vue
@@ -6,10 +6,23 @@
         <div class="card-content-changelog white-text" style="margin:1,2%">
             <span class="card-title-changelog" style="margin-bottom: 0;">SlayBot Version 0.1.5</span>
             <span class="discord-text text-greyple" style="margin-bottom: 8px; display: block;">22nd of August 2020</span>
+            <p><i class="fas fa-plus"></i> Added</p>
+            <p class="cl-desc">
+            • The <code>shards</code> command has been implemented to the <b>Core</b> category.<br>
+            • The <code>8ball</code> command has been implemented to the <b>Fun</b> category.<br>
+            • The <code>banner</code> command has been implemented to the <b>Fun</b> category.
+            </p>
+
+            <p><i class="fas fa-edit"></i> Updated</p>
+            <p class="cl-desc">
+            • Added some aliases to the <code>help</code> command
+            </p>
+
             <p><i class="fas fa-wrench"></i> Fixed</p>
             <p class="cl-desc">
             • Fixed incorrect statistics on the <code>stats</code> command.
             </p>
+
             <p><i class="fas fa-exclamation-triangle"></i> Known Issues</p>
             <p>
             • The presences are still showing incorrect amount of servers or users. Please use <code>stats</code> command to get the correct stats
@@ -20,7 +33,7 @@
         <div class="card-content-changelog white-text" style="margin:1,2%">
             <span class="card-title-changelog" style="margin-bottom: 0;">SlayBot Version 0.1.4</span>
             <span class="discord-text text-greyple" style="margin-bottom: 8px; display: block;">18th of August 2020</span>
-            <p><i class="fas fa-edit"></i> Updates</p>
+            <p><i class="fas fa-edit"></i> Updated</p>
             <p>
             • Added Environment field to the <code>stats</code> command
             </p>
