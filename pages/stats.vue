a9d5ce17f4f9f9c1f15b0f178d1d91eb61f1bdb0
Slayer <aleksi.klen@gmail.com>
2020-10-26 14:53:08 +0200

    Update the theme of stats page

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <iframe src="https://p.datadoghq.com/sb/3l9dqcu1g0zvv4vo-ab514f956a0d370603d938ddd03d6503?tv_mode=false" class="frame"></iframe>
+    <iframe src="https://p.datadoghq.com/sb/3l9dqcu1g0zvv4vo-ab514f956a0d370603d938ddd03d6503?theme=dark" class="frame"></iframe>
   </section>
 </template>
 
