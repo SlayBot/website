40e6c8f73d6faee0b1bf22203b0efb233f243b9e
Slayer <51095261+GhostSlayer@users.noreply.github.com>
2020-11-05 12:06:41 -0800

    idk where to put this lmfao nuxt.js is shit

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,16 @@
   </div>
 </template>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9DRDGJ4579"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9DRDGJ4579');
+</script>
+
 <style lang="scss">
   @import "../assets/main";
   @import "../assets/styles";
